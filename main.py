44815ee3bd76fea8b8fc580314349e9f5be94dd1
zacharygilliom <zacharygilliom@gmail.com>
2020-02-01 04:44:48 -0500

    Cleaned the data and added a visual.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+def strip_val(val):
+	val[1] = val[1].replace('\xa0', '')
+	return val
 
 website_url = requests.get('https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita').text
 
@@ -16,9 +22,9 @@ for table in my_table:
 	rows = []
 	for i in range(1, len(links)):
 		val = links[i].text
-		val = val.replace('\n', ',')
-		val = val.replace('\xa0', '')
-		val = [val[1:-1]]
+		val = val.split('\n')
+		val = val[1:-1]
+		val = strip_val(val)
 		rows.append(val)
 	data.append(rows)	
 
@@ -26,8 +32,40 @@ International_Monetary_Fund_2019_data = data[0]
 World_Bank_2018_data = data[1]
 United_Nations_2017_data = data[2]
 
-International_Monetary_Fund_2019 = pd.DataFrame(columns=['Rank', 'Country', 'GDP'])
 
-print(International_Monetary_Fund_2019)
+International_Monetary_Fund_2019 = pd.DataFrame(columns=['Rank', 'Country', 'IMF GDP'], data=International_Monetary_Fund_2019_data)
+
+World_Bank_2018 = pd.DataFrame(columns=['Rank', 'Country', 'World Bank GDP'], data=World_Bank_2018_data)
+
+United_Nations_2017 = pd.DataFrame(columns=['Rank', 'Country', 'UN GDP'], data=United_Nations_2017_data)
+
+# United_Nations_2017['Rank'] = United_Nations_2017['Rank'].replace('-', 0)
+
+United_Nations_2017.drop_duplicates('Rank', inplace=True)
+
+United_Nations_2017.drop(3, inplace=True)
+United_Nations_2017.drop(96, inplace=True)
 
 
+United_Nations_2017 = United_Nations_2017.astype({'Rank': 'str', 'UN GDP': 'str'}).astype({'Rank':'int'})
+
+print(United_Nations_2017.dtypes)
+
+for index, row in United_Nations_2017.iterrows():
+	United_Nations_2017.loc[index, 'UN GDP'] = row['UN GDP'].replace(',', '')
+	# print(row['UN GDP'])
+
+United_Nations_2017 = United_Nations_2017.astype({'UN GDP': 'int'})
+# print(United_Nations_2017)
+# print(United_Nations_2017.dtypes)
+
+
+# print(International_Monetary_Fund_2019_data)
+# print(International_Monetary_Fund_2019)
+# print(World_Bank_2018)
+# print(United_Nations_2017)
+
+plt.subplot(311)
+sns.lineplot(x=United_Nations_2017[United_Nations_2017['Rank'] < 11]['Country'], y=United_Nations_2017[United_Nations_2017['Rank'] < 11]['UN GDP'])
+
+plt.show()
